78cf780b03ac4b4e30f9400eb5efeb12ace18fad
Gareth Tribello <gareth.tribello@gmail.com>
2020-11-15 12:55:21 +0000

    Create main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+def correlation_function( spins, r ) :
+  # Your code goes here
+  
+  return 
+
+spins1 = [1,-1,1,1,-1,1,1,1,-1,-1]
+print( correlation_function( spins1, 1 ), correlation_function( spins1, 2 ), correlation_function( spins1, 3 ) )
+spins2 = [-1,1,1,1,-1,-1,1,1,1,-1]
+print( correlation_function( spins2, 2 ), correlation_function( spins2, 2 ), correlation_function( spins2, 3 ) )
