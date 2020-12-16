0a720cfae09afc93495ba4143ed7b9a337f718b0
Gareth Tribello <gareth.tribello@gmail.com>
2020-12-16 14:57:48 +0000

    Added answers

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 def correlation_function( spins, r ) :
   # Your code goes here
-  
-  return 
+  av2, var, average = 0, 0, sum(spins) / len(spins) 
+  for i in range(len(spins)) : 
+    av2 = av2 + (spins[i] - average)*(spins[(i+r)%len(spins)] - average )
+    var = var + (spins[i] - average)*(spins[i] - average) 
+  return av2/var
 
 spins1 = [1,-1,1,1,-1,1,1,1,-1,-1]
 print( correlation_function( spins1, 1 ), correlation_function( spins1, 2 ), correlation_function( spins1, 3 ) )
